f6589295e82d16d167fc5e96a9719a61ed443af1
Thiago Valls <olavalls@gmail.com>
2020-05-15 23:35:08 -0300

    configuring eslint and prettier to getting standardized

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
 
 const App: React.FC = () => {
-    return (
-        <View>
-            <Text>Hello</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
 
 export default App;
