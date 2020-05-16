59294e65d63a79ed53715f29bba1f3f0ad3f526e
Thiago Valls <olavalls@gmail.com>
2020-05-16 00:33:20 -0300

    Installing react-native-navigation and navigation/stack to create navigation on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
